096cfedfe2a9437fdffc77b29124dab276df8476
adrian <adrianfj@gmail.com>
2019-03-20 01:11:43 -0400

    Improve format of printed output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import helpers as hp
+import json
 
 # Import recipe specific scrappers
 from recipe_scrappers import factory
@@ -24,7 +25,8 @@ def get_this_recipe(url):
 
     if recipe_info:
         print("# Sucess!")
-        print(recipe_info)
+        pretty_data = json.dumps(recipe_info, indent=4)
+        print(pretty_data)
         return recipe_info
     else:
         print("Error!")
@@ -34,7 +36,8 @@ def get_this_recipe(url):
     Run Script
 """
 # Recipe url (Feel free to change the test URL)
-TEST_URL = 'https://www.allrecipes.com/recipe/60598/vegetarian-korma/?internalSource=hub%20recipe&referringContentType=Search'
+TEST_URL = 'https://www.allrecipes.com/recipe/257938/spicy-thai-basil-chicken-pad-krapow-gai/?clickId=right%20rail1&internalSource=rr_feed_recipe_sb&referringId=60598%20referringContentType%3Drecipe'
+#'https://www.allrecipes.com/recipe/60598/vegetarian-korma/?internalSource=hub%20recipe&referringContentType=Search'
 
 # Request recipe! 
 get_this_recipe(TEST_URL)
